01a7bb9f9888250f67abdd66e5f22077c3ad3912
Sebastian Granados <60801979+GranadosSebastian@users.noreply.github.com>
2022-12-10 18:06:34 -0600

    Add files via upload

---
+++ src/Data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Java Project',
+        imgUrl: '/assets/javaspring.jpg',
+        stack: ['Java', 'Spring', 'JavaScript'],
+        link: 'https://google.com',
+    },
+    {
+        title: 'Portfolio Website',
+        imgUrl: '/assets/website.jpg',
+        stack: ['HTML/CSS', 'Javascript', 'React'],
+        link: 'https://google.com',
+    },
+    {
+        title: 'Games',
+        imgUrl: '/assets/games.jpg',
+        stack: ['HTML', 'CSS', 'JavaScript'],
+        link: 'https://google.com',
+    },
+]
